2fce87bfb6229e74bb485a9f26185a558a403d64
SabrinaIslam <44192086+SabrinaIslam@users.noreply.github.com>
2018-10-17 13:56:55 +1100

    Update test_git.py
    added sample file 1 and 2 and uncommented them

--- test_git.py
+++ test_git.py
@@ -2,13 +2,26 @@ In git, new lines are green.
 I am going to add this line.
 
 def addition(a, b):
-
   summation = a + b
-
   return summation
-
 def subtraction(a, b):
-
-difference = a - b
-
-return difference
+  difference = a - b
+  return difference
+def division(a, b):
+  quotient = a / b
+  return quotient
+def multiplication(a, b):
+  product = a * b
+  return product
+def bad_addition(a, b):
+  summation = a + b + 0.002
+  return summation
+def bad_subtraction(a, b):
+  difference = a - b + 7
+  return difference
+def bad_division(a, b):
+  quotient = (a+13) / b
+  return quotient
+def bad_multiplication(a, b):
+  product = a * b + 12
+  return product
